c9f931a0e42b3169db6a55b600d98d84bda331d0
erika <olivoerikac@gmail.com>
2018-11-13 19:35:09 -0300

    se cambio el titulo Laravel 5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Laravel 5 erika</div>
+				<div class="title">olivo</div>
 				<div class="quote">{{ Inspiring::quote() }}</div>
 			</div>
 		</div>
